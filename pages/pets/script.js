826b3b90d6fdb4d249de91848398a318eb7c29be
Valentin Tur <valentin.tur.it@gmail.com>
2022-06-02 17:06:49 +0200

    feat: add overflow for mobile menus

--- pages/pets/script.js
+++ pages/pets/script.js
@@ -2,12 +2,27 @@ const headerBlockElem = document.querySelector('.header__block');
 const headerLogoElem = document.querySelector('.header__logo');
 const navigationMobileElem = document.querySelector('.navigation__mobile');
 const menuButtonElem = document.querySelector('#menu-toggle');
+const overflow = document.createElement('div');
+
+const createOverflowElem = () => {
+    overflow.classList.add('overflow');
+    overflow.style.width = '100%';
+    overflow.style.height = '100vh';
+    overflow.style.position = 'absolute';
+    overflow.style.top = '0';
+    overflow.style.backgroundColor = '#292929';
+    overflow.style.opacity = '0.6';
+    overflow.style.zIndex = '5';
+    document.body.prepend(overflow);
+}
 
 const moveLogoInsideMobileMenu = () => {
     if (menuButtonElem.checked == 1) {
         navigationMobileElem.prepend(headerLogoElem);
+        createOverflowElem();
     } else {
         headerBlockElem.prepend(headerLogoElem);
+        overflow.remove()
     }
     
 }
